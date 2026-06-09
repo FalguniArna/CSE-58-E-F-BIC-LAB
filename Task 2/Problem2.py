4ffcc3b9cf52318c001cd1ad1d42cddae171c47c
Falguni Datta Arna <145069356+FalguniArna@users.noreply.github.com>
2026-06-09 16:10:07 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,10 @@
+str = input()
+k=int(input())
+freq={}
+for i in range(len(str)-k+1):
+  patt=str[i:i+k]
+  freq[patt]=freq.get(patt,0)+1
 
+mx_c=max(freq.values())
+res=[patt for patt, count in freq.items() if count==mx_c]
+print(*res)
