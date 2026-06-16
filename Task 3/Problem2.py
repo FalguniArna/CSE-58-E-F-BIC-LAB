68c7d63b479703199bc2999e0617ed105f550f1e
Falguni Datta Arna <145069356+FalguniArna@users.noreply.github.com>
2026-06-16 16:21:36 +0600

    Update Problem2.py

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,15 @@
-
+str1=input().strip()
+str2=input()
+d=int(input())
+v=[]
+k=len(str1)
+for i in range(len(str2)-k+1):
+        c=0
+        for j in range(k):
+            if str1[j] != str2[i+j]:
+                c+=1
+            
+        if c<=d:
+                #v.append(str(i))
+            print(str(i), end = " ")
+                
